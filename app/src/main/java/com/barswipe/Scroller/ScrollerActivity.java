46ff310203ffcb8af19d313140710233caae8768
SoLi <353124127@qq.com>
2016-05-25 17:12:41 +0800

    offsetLeftAndRight offsetTopAndButtom学习

--- app/src/main/java/com/barswipe/Scroller/ScrollerActivity.java
+++ app/src/main/java/com/barswipe/Scroller/ScrollerActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.barswipe.R;
 
@@ -13,7 +12,7 @@ import com.barswipe.R;
  * Created by SoLi on 2016/5/24.
  */
 public class ScrollerActivity extends Activity implements View.OnClickListener {
-    private TextView tv;
+    private View tv;
     private Button bt_scrollLeft;
     private Button bt_scrollRight;
 
@@ -22,12 +21,13 @@ public class ScrollerActivity extends Activity implements View.OnClickListener {
 
         setContentView(R.layout.main);
 
-        tv = (TextView) findViewById(R.id.tv_scroll);
+        tv =  findViewById(R.id.tv_scroll);
 
         bt_scrollLeft = (Button) findViewById(R.id.bt_scrollLeft);
         bt_scrollRight = (Button) findViewById(R.id.bt_scrollRight);
 
         findViewById(R.id.bt_scrollBy).setOnClickListener(this);
+        findViewById(R.id.bt_offset).setOnClickListener(this);
         bt_scrollLeft.setOnClickListener(this);
         bt_scrollRight.setOnClickListener(this);
     }
@@ -37,20 +37,24 @@ public class ScrollerActivity extends Activity implements View.OnClickListener {
 
         switch (v.getId()) {
             case R.id.bt_scrollLeft:
-                tv.scrollBy(20, 0);
+                findViewById(R.id.test).scrollBy(20, 0);
                 break;
             case R.id.bt_scrollRight:
-                tv.scrollTo(-100, 0);
+                findViewById(R.id.test).scrollTo(-100, 0);
                 break;
             case R.id.bt_scrollBy:
-                tv.scrollBy(-30, -30);
+                findViewById(R.id.test).scrollBy(-30, -30);
                 break;
             case R.id.startScrooler:
                 startActivity(new Intent(this,MultiScreenActivity.class));
                 break;
+            case R.id.bt_offset:
+                tv.offsetLeftAndRight(10);
+                tv.offsetTopAndBottom(10);
+                break;
         }
-        int tvscrllX = tv.getScrollX();
-        int tvscrllY = tv.getScrollY();
+        int tvscrllX = findViewById(R.id.test).getScrollX();
+        int tvscrllY = findViewById(R.id.test).getScrollY();
         System.out.println(" tvscrllX ---> " + tvscrllX + " --- tvscrllY ---> " + tvscrllY);
     }
 }
