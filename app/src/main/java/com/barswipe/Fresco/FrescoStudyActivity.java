0f557bfe83bc4435e096df91ec2cddad86a5c95e
Sofia4041 <353124127@qq.com>
2016-07-26 21:32:25 +0800

    Fresco Study

---
+++ app/src/main/java/com/barswipe/Fresco/FrescoStudyActivity.java
@@ -0,0 +1,18 @@
+package com.barswipe.Fresco;
+
+import android.os.Bundle;
+
+import com.barswipe.BaseActivity;
+import com.barswipe.R;
+
+/**
+ * Created by soli on 7/26/16.
+ */
+public class FrescoStudyActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fresco_study_layout);
+    }
+}
