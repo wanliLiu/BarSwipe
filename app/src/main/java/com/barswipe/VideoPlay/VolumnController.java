d721fb6025e20f2a4b812bf66a47ca9def47d442
liuwanli <liuwanli50>
2016-10-26 17:24:37 +0800

    VideoView播放网络视频

---
+++ app/src/main/java/com/barswipe/VideoPlay/VolumnController.java
@@ -0,0 +1,34 @@
+package com.barswipe.VideoPlay;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import com.barswipe.R;
+
+
+public class VolumnController {
+    private Toast t;
+    private VolumnView tv;
+
+    private Context context;
+
+    public VolumnController(Context context) {
+        this.context = context;
+    }
+
+    public void show(float progress) {
+        if (t == null) {
+            t = new Toast(context);
+            View layout = LayoutInflater.from(context).inflate(R.layout.item_video_voice, null);
+            tv = (VolumnView) layout.findViewById(R.id.volumnView);
+            t.setView(layout);
+            t.setGravity(Gravity.CENTER, 0, 0);
+            t.setDuration(Toast.LENGTH_SHORT);
+        }
+        tv.setProgress(progress);
+        t.show();
+    }
+}
