3872c65563dc6a760a07c12dc38d1bf0e1adfcc1
liuwanli <liuwanli50>
2016-11-11 16:01:56 +0800

    gridlayout study

--- app/src/main/java/com/barswipe/GridLayoutStudy.java
+++ app/src/main/java/com/barswipe/GridLayoutStudy.java
@@ -2,15 +2,42 @@ package com.barswipe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayout;
+import android.view.View;
+
+import com.jakewharton.rxbinding.view.RxView;
+
+import rx.functions.Action1;
 
 /**
  * http://www.cnblogs.com/Chenshuai7/p/5321766.html
  */
 public class GridLayoutStudy extends AppCompatActivity {
 
+
+    private int index = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_layout_study);
+
+        RxView.clicks(findViewById(R.id.test))
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        dd();
+                    }
+                });
+    }
+
+
+    private void dd() {
+        index = (index + 1) % 7;
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.grd);
+        View view = gridLayout.getChildAt(7);
+        GridLayout.LayoutParams params = (GridLayout.LayoutParams) view.getLayoutParams();
+        params.columnSpec = GridLayout.spec(index, GridLayout.CENTER, 0f);
+        view.setLayoutParams(params);
     }
 }
