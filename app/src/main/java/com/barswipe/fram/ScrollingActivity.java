bcbbacde3c870e225e9ce145073a979daa935350
Sofia4041 <353124127@qq.com>
2016-11-11 22:45:55 +0800

    NestScrollView 类似的可以控制事件传递   setNestedScrollingEnabled  AppBarLayout.setExpanded

--- app/src/main/java/com/barswipe/fram/ScrollingActivity.java
+++ app/src/main/java/com/barswipe/fram/ScrollingActivity.java
@@ -1,8 +1,10 @@
 package com.barswipe.fram;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,6 +12,8 @@ import android.view.View;
 import com.barswipe.R;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    private int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +23,27 @@ public class ScrollingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                NestedScrollView ds = (NestedScrollView) findViewById(R.id.nestScrollView);
+                AppBarLayout app_bar = (AppBarLayout) findViewById(R.id.app_bar);
+                if (index == 0) {
+                    ds.setNestedScrollingEnabled(false);
+                } else if (index == 1) {
+                    ds.setNestedScrollingEnabled(true);
+                } else if (index == 2) {
+                    app_bar.setExpanded(true, true);
+                } else if (index == 3) {
+                    app_bar.setExpanded(false, true);
+                }
+                index = (index + 1) % 4;
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 }
