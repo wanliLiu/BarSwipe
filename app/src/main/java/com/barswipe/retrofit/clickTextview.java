0525c4c215613cdb3f1f46b1beb4cf73782a7732
liuwanli <liuwanli50>
2016-12-01 15:28:29 +0800

    SpannableString 图片按压背景效果

--- app/src/main/java/com/barswipe/retrofit/clickTextview.java
+++ app/src/main/java/com/barswipe/retrofit/clickTextview.java
@@ -1,11 +1,18 @@
 package com.barswipe.retrofit;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.TextPaint;
 import android.text.style.ClickableSpan;
+import android.text.style.ImageSpan;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -44,14 +51,30 @@ public class clickTextview extends TextView {
         setText(emojiString);
 
         //这句话很重要
+        /**用自定义这个 点击图片友按压效果没有**/
         LocalLinkMovementMethod method = LocalLinkMovementMethod.getInstance();
         method.setClickColor(getResources().getColor(R.color.colorPrimary_press));
         setMovementMethod(method);
+        /**用默认 点击图片友按压效果**/
 //        setMovementMethod(LinkMovementMethod.getInstance());
         append("开始到好似看电视了贷款 ");
 
-        SpannableString sp = new SpannableString("@许嵩");
-        sp.setSpan(new clickSpan("@许嵩"), 0, "@许嵩".length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);//SPAN_EXCLUSIVE_INCLUSIVE SPAN_EXCLUSIVE_EXCLUSIVE
+        String temp = "   " + " " + "@许嵩";
+        SpannableString sp = new SpannableString(temp);
+        Drawable d = ContextCompat.getDrawable(getContext(), R.mipmap.icon_see_image);
+        int pannding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1.0f, getResources().getDisplayMetrics());
+        d.setBounds(0, 0 - pannding, d.getIntrinsicWidth() - pannding, d.getIntrinsicHeight() - pannding);
+        ClickableImageSpan span = new ClickableImageSpan(d) {
+            @Override
+            public void onClick(View view) {
+//                //点击图片
+//                if (listener != null) {
+//                    listener.OnClick(view);
+//                }
+            }
+        };
+        sp.setSpan(span, 2, 3, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);//图片
+        sp.setSpan(new clickSpan(temp), 0, temp.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);//SPAN_EXCLUSIVE_INCLUSIVE SPAN_EXCLUSIVE_EXCLUSIVE
         append(sp);
 
         append("有开始了哦 sssssssssssssssssssss不是慢");
@@ -61,6 +84,46 @@ public class clickTextview extends TextView {
         s23p.setSpan(new clickSpan(tes), 0, tes.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);//SPAN_EXCLUSIVE_INCLUSIVE SPAN_EXCLUSIVE_EXCLUSIVE
         append(s23p);
         append("  内容开始了哦");
+    }
+
+    /**
+     * 默认的图片点击是没有事件的，这里我们自己添加事件
+     */
+    public static abstract class ClickableImageSpan extends ImageSpan {
+
+
+        public ClickableImageSpan(Drawable b) {
+            super(b);
+        }
+
+        public ClickableImageSpan(Context context, Bitmap b) {
+            super(context, b);
+        }
+
+        public ClickableImageSpan(Drawable d, int verticalAlignment) {
+            super(d, verticalAlignment);
+        }
+
+        /**
+         * 让图片居中 start
+         */
+        @Override
+        public void draw(Canvas canvas, CharSequence text, int start, int end,
+                         float x, int top, int y, int bottom, Paint paint) {
+            Drawable b = getDrawable();
+            // font metrics of text to be replaced
+            Paint.FontMetricsInt fm = paint.getFontMetricsInt();
+            int transY = (y + fm.descent + y + fm.ascent) / 2
+                    - b.getBounds().bottom / 2;
+
+            canvas.save();
+            canvas.translate(x, transY);
+            b.draw(canvas);
+            canvas.restore();
+
+        }
+
+        public abstract void onClick(View view);
     }
 
     /**
