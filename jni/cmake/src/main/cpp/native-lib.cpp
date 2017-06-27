c71c2e30c95b92362622b362f9e7d190d4723714
liuwanli <liuwanli50>
2017-06-27 10:23:33 +0800

    fix

--- jni/cmake/src/main/cpp/native-lib.cpp
+++ jni/cmake/src/main/cpp/native-lib.cpp
@@ -8,5 +8,5 @@ Java_com_example_soli_jnitest_MainActivity_stringFromJNI(
         jobject /* this */) {
     __android_log_print(ANDROID_LOG_ERROR, "ProjectName", "native make 通过cmake来做\nnative有两种方式，一是传统的jni方式，另一种是cmake来做，google官方后面可能会用这个作为官方的native开发，jni方式可能会去掉");
     __android_log_print(ANDROID_LOG_ERROR, "ProjectName", "cmake方式要了解CMakeLists.txt的编写，语法等");
-    return env->NewStringUTF("Hello from C++");
+    return env->NewStringUTF("Hello from C++,来自底层库函数");
 }
