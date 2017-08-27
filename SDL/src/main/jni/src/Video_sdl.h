20bc8a41679e687dc8df0974cc976f849b10bb92
Sofia4041 <353124127@qq.com>
2017-08-27 16:42:33 +0800

    audio and video

---
+++ SDL/src/main/jni/src/Video_sdl.h
@@ -0,0 +1,11 @@
+//
+// Created by SOli on 27/08/2017.
+//
+
+#ifndef BARSWIPE_VIDEO_SDL_H
+#define BARSWIPE_VIDEO_SDL_H
+
+
+extern int VideoSDL_play(int argc, char *argv[]);
+
+#endif //BARSWIPE_VIDEO_SDL_H
--- SDL/src/main/jni/src/main.c
+++ SDL/src/main/jni/src/main.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 
 #include "SDL.h"
+#include <android/log.h>
+
+#include "Video_sdl.h"
+
+
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"SDL_study", __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,"SDL_study", __VA_ARGS__)
 
 typedef struct Sprite {
     SDL_Texture *texture;
@@ -48,7 +55,13 @@ void draw(SDL_Window *window, SDL_Renderer *renderer, const Sprite sprite) {
     SDL_RenderDrawLine(renderer, 0, h / 2, w, h / 2);
 }
 
-int main(int argc, char *argv[]) {
+/**
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
+int displayBmp(int argc, char *argv[]) {
 
     SDL_Window *window;
     SDL_Renderer *renderer;
@@ -83,8 +96,228 @@ int main(int argc, char *argv[]) {
 
         SDL_Delay(10);
     }
-    //这里相当于java 的System.exit(0);
-//    exit(0);
 
+
+    //这里相当于java 的System.exit(0);
+    exit(0);
+}
+
+//*******************************Audio play************************/
+
+//Buffer:
+//|-----------|-------------|
+//chunk-------pos---len-----|
+static Uint8 *audio_chunk;
+static Uint32 audio_len;
+static Uint8 *audio_pos;
+
+/* Audio Callback
+ * The audio function callback takes the following parameters:
+ * stream: A pointer to the audio buffer to be filled
+ * len: The length (in bytes) of the audio buffer
+ *
+*/
+void fill_audio(void *udata, Uint8 *stream, int len) {
+    //SDL 2.0
+    SDL_memset(stream, 0, len);
+    if (audio_len == 0)        /*  Only  play  if  we  have  data  left  */
+        return;
+    len = (len > audio_len ? audio_len : len);    /*  Mix  as  much  data  as  possible  */
+
+    SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);
+    audio_pos += len;
+    audio_len -= len;
+}
+
+/**
+ * 播放音频
+ * @param argv
+ * @param argv
+ * @return
+ */
+int playPCMAudio(int argc, char *argv[]) {
+    //Init
+    if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
+        LOGE("Could not initialize SDL - %s\n", SDL_GetError());
+        return -1;
+    }
+    //SDL_AudioSpec
+    SDL_AudioSpec wanted_spec;
+    wanted_spec.freq = 44100;
+    wanted_spec.format = AUDIO_S16SYS;
+    wanted_spec.channels = 2;
+    wanted_spec.silence = 0;
+    wanted_spec.samples = 1024;
+    wanted_spec.callback = fill_audio;
+
+    if (SDL_OpenAudio(&wanted_spec, NULL) < 0) {
+        LOGE("can't open audio.\n");
+        return -1;
+    }
+
+    FILE *fp = fopen("/storage/emulated/0/convexd/audio.pcm", "rb+");
+    if (fp == NULL) {
+        LOGE("cannot open this file\n");
+        return -1;
+    }
+    //For YUV420P
+    int pcm_buffer_size = 4096;
+    char *pcm_buffer = (char *) malloc(pcm_buffer_size);
+    int data_count = 0;
+
+
+    Uint8 done = 0;
+    SDL_Event event;
+    while (!done) {
+
+        /* Check for events */
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT || event.type == SDL_KEYDOWN ||
+                event.type == SDL_FINGERDOWN) {
+                done = 1;
+            }
+        }
+
+        if (fread(pcm_buffer, 1, pcm_buffer_size, fp) != pcm_buffer_size) {
+            // Loop
+            fseek(fp, 0, SEEK_SET);
+            fread(pcm_buffer, 1, pcm_buffer_size, fp);
+            data_count = 0;
+        }
+        LOGE("Now Playing %10d Bytes data.\n", data_count);
+        data_count += pcm_buffer_size;
+        //Set audio buffer (PCM data)
+        audio_chunk = (Uint8 *) pcm_buffer;
+        //Audio buffer length
+        audio_len = pcm_buffer_size;
+        audio_pos = audio_chunk;
+        //Play
+        SDL_PauseAudio(0);
+        while (audio_len > 0)//Wait until finish
+            SDL_Delay(1);
+
+
+    }
+
+    return 0;
+}
+
+/**
+ *
+ * @param argc
+ * @param argv
+ */
+void displayAll(int argc, char *argv[]) {
+    //Init
+    if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
+        LOGE("Could not initialize SDL - %s\n", SDL_GetError());
+        return;
+    }
+
+
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    if (SDL_CreateWindowAndRenderer(0, 0, 0, &window, &renderer) < 0)
+        exit(2);
+
+    Sprite sprite = LoadSprite("image.bmp", renderer);
+    if (sprite.texture == NULL)
+        exit(2);
+
+
+    //SDL_AudioSpec
+    SDL_AudioSpec wanted_spec;
+    wanted_spec.freq = 44100;
+    wanted_spec.format = AUDIO_S16SYS;
+    wanted_spec.channels = 2;
+    wanted_spec.silence = 0;
+    wanted_spec.samples = 1024;
+    wanted_spec.callback = fill_audio;
+
+    if (SDL_OpenAudio(&wanted_spec, NULL) < 0) {
+        LOGE("can't open audio.\n");
+        return;
+    }
+
+    FILE *fp = fopen("/storage/emulated/0/convexd/audio.pcm", "rb+");
+    if (fp == NULL) {
+        LOGE("cannot open this file\n");
+        return;
+    }
+    //For YUV420P
+    int pcm_buffer_size = 4096;
+    char *pcm_buffer = (char *) malloc(pcm_buffer_size);
+    int data_count = 0;
+
+
+    Uint8 done = 0;
+    SDL_Event event;
+    while (!done) {
+
+
+        /* Draw a gray background */
+        SDL_SetRenderDrawColor(renderer, 0xC8, 0x64, 0x21, 0x1A);//C864211A
+        SDL_RenderClear(renderer);
+
+        draw(window, renderer, sprite);
+
+        /* Update the screen! */
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(10);
+
+
+        /* Check for events */
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT || event.type == SDL_KEYDOWN ||
+                event.type == SDL_FINGERDOWN) {
+                done = 1;
+            }
+        }
+
+        if (fread(pcm_buffer, 1, pcm_buffer_size, fp) != pcm_buffer_size) {
+            // Loop
+            fseek(fp, 0, SEEK_SET);
+            fread(pcm_buffer, 1, pcm_buffer_size, fp);
+            data_count = 0;
+        }
+        LOGE("Now Playing %10d Bytes data.\n", data_count);
+        data_count += pcm_buffer_size;
+        //Set audio buffer (PCM data)
+        audio_chunk = (Uint8 *) pcm_buffer;
+        //Audio buffer length
+        audio_len = pcm_buffer_size;
+        audio_pos = audio_chunk;
+        //Play
+        SDL_PauseAudio(0);
+        while (audio_len > 0)//Wait until finish
+            SDL_Delay(1);
+    }
+
+    exit(0);
+}
+
+
+#define DisplayBmp 0
+#define AudioPlay 0
+#define VideoPlay 1
+
+/**
+ * 入口函数
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char *argv[]) {
+#if VideoPlay
+    VideoSDL_play(argc, argv);
+#elif  DisplayBmp
+    displayBmp(argc, argv);
+#elif AudioPlay
+    playPCMAudio(argc, argv);
+#else
+    displayAll(argc, argv);
+#endif
     return 0;
 }
