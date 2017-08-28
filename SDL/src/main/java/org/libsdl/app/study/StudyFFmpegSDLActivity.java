beb6ae2980b11e78822c55fa05ba6351fccc193a
Sofia4041 <353124127@qq.com>
2017-08-28 23:48:03 +0800

    keep going

---
+++ SDL/src/main/java/org/libsdl/app/study/StudyFFmpegSDLActivity.java
@@ -0,0 +1,34 @@
+package org.libsdl.app.study;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import org.libsdl.app.R;
+import org.libsdl.app.SDLActivity;
+
+/**
+ * Created by soli on 28/08/2017.
+ */
+
+public class StudyFFmpegSDLActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    /**
+     * @param view
+     */
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sdlActivity:
+                startActivity(new Intent(this, SDLActivity.class));
+                break;
+        }
+    }
+}
--- SDL/src/main/jni/src/Android.mk
+++ SDL/src/main/jni/src/Android.mk
@@ -1,5 +1,92 @@
 LOCAL_PATH := $(call my-dir)
 
+
+###########################
+#
+# FFmpeg shared library
+#
+###########################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:= avcodec
+
+LOCAL_SRC_FILES:= lib/libavcodec-57.so
+
+LOCAL_EXPORT_C_INCLUDES:= $(LOCAL_PATH)/include
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:= avformat
+
+LOCAL_SRC_FILES:= lib/libavformat-57.so
+
+LOCAL_EXPORT_C_INCLUDES:= $(LOCAL_PATH)/include
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:= swscale
+
+LOCAL_SRC_FILES:= lib/libswscale-4.so
+
+LOCAL_EXPORT_C_INCLUDES:= $(LOCAL_PATH)/include
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:= avutil
+
+LOCAL_SRC_FILES:= lib/libavutil-55.so
+
+LOCAL_EXPORT_C_INCLUDES:= $(LOCAL_PATH)/include
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:= avfilter
+
+LOCAL_SRC_FILES:= lib/libavfilter-6.so
+
+LOCAL_EXPORT_C_INCLUDES:= $(LOCAL_PATH)/include
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:= swresample
+
+LOCAL_SRC_FILES:= lib/libswresample-2.so
+
+LOCAL_EXPORT_C_INCLUDES:= $(LOCAL_PATH)/include
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := main
